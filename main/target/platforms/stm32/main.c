5cea8e9ff602f74a1623e40705e867132844a0e6
Jacob Walser <jwalser90@gmail.com>
2019-01-09 21:18:35 -0500

    attempt to set gpio

--- main/target/platforms/stm32/main.c
+++ main/target/platforms/stm32/main.c
@@ -1,5 +1,5 @@
 #include <os/os.h>
-
+#include <hal/gpio.h>
 extern void app_main(void);
 
 #warning temp fix
@@ -9,6 +9,7 @@ void assert(int c)
 
 int main(void)
 {
+    hal_gpio_set_level(HAL_PC(13), HAL_GPIO_LOW);
     app_main();
     vTaskStartScheduler();
 }
